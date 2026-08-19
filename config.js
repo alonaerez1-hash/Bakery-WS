e1c0f1d61e9c8eae908ba12a804de5c4bf58c3a8
alonaerez1-hash <alonaerez1@gmail.com>
2026-08-19 11:03:51 +0300

    Connect Bakery WS to dedicated Supabase project

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.BAKERY_WS_CONFIG={
-  supabaseUrl:'',
-  supabasePublishableKey:'',
+  supabaseUrl:'https://hazcuzbiawnfzgmaeayf.supabase.co',
+  supabasePublishableKey:'sb_publishable_qWxKy_Lz165_HFuUEbNkOw_Y6vBh56W',
   billingEnabled:false
 };
