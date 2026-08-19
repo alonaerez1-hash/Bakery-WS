ab3a06952485ad24cba4c97a2111dd63f6774f5e
alonaerez1-hash <alonaerez1@gmail.com>
2026-08-19 11:45:14 +0300

    Fix Bakery WS email confirmation redirects
    * Set production app URL for auth redirects
    
    * Fix email confirmation redirects and resend flow
    
    * Add resend confirmation control
    
    * Guard production auth redirect and resend flow

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 window.BAKERY_WS_CONFIG={
   supabaseUrl:'https://hazcuzbiawnfzgmaeayf.supabase.co',
   supabasePublishableKey:'sb_publishable_qWxKy_Lz165_HFuUEbNkOw_Y6vBh56W',
+  appUrl:'https://alonaerez1-hash.github.io/Bakery-WS/',
   billingEnabled:false
 };
